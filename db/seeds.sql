58d44bf3fc11aec128f24ff2146992bc2a588ecb
annajantzen <aejantz2015@gmail.com>
2024-03-13 17:24:41 -0500

    added to the seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name)
+VALUES
+('Sales')
+('Finance')
+('Engineering')
+('Legal')
+
+INSERT INTO position (title, department_id, salary)
+('Sales Lead', 1 100000)
+('Salesperson', 1, 80000)
+('Account Manager', 2, 150000)
+('Accountant', 2, 100000)
+('Lead Engineer', 3, 150000)
+('Software Engineer', 3, 120000)
+('Lawyer', 4, 200000)
+('Paralegal', 4, 100000)
+
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
